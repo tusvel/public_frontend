54a5234180e1a77e4682ad94692f4db66283d1dd
tusvel <tuswells@gmail.com>
2023-07-15 18:19:54 +0300

    2.3 Webpack dev server. Переменные окружения env.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {someFn} from "./test";
 
 someFn(123)
+
+document.body.innerHTML = "<div>Hello world <div>"
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,25 @@
 import webpack from "webpack";
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
 
-const paths: BuildPaths = {
-	entry: path.resolve(__dirname, 'src', 'index.ts'),
-	build: path.resolve(__dirname, 'build'),
-	html: path.resolve(__dirname, 'public', 'index.html')
+export default (env: BuildEnv) => {
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, 'src', 'index.ts'),
+		build: path.resolve(__dirname, 'build'),
+		html: path.resolve(__dirname, 'public', 'index.html')
+	}
+
+	const mode = env.mode || "development"
+	const isDev = mode === "development"
+	const port = env.port || 3000
+
+	const config: webpack.Configuration = buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port
+	})
+
+	return config;
 }
-
-const mode = "development"
-const isDev = mode === "development"
-
-const config: webpack.Configuration = buildWebpackConfig({
-	mode: 'development',
-	paths,
-	isDev
-})
-
-export default config;
