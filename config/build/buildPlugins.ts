8ec4a232cf74d39dba73880fa180e1434ad30c2d
tusvel <tuswells@gmail.com>
2023-07-29 14:51:09 +0300

    3.27 CI pipeline. Автоматизация прогона тестов.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,7 +6,7 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -18,10 +18,16 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     new webpack.DefinePlugin({
       __IS_DEV__: isDev,
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin(),
-    new BundleAnalyzerPlugin({
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
       openAnalyzer: false,
     }),
-  ];
+    );
+  }
+
+  return plugins;
 }
