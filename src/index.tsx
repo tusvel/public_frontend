3c2f38d66ce47321273cea6f4d116b37aec0b16f
tusvel <tuswells@gmail.com>
2023-08-19 00:01:43 +0300

    add font

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import 'app/styles/fonts/font.scss';
 import 'app/styles/index.scss';
 import { StoreProvider } from 'app/providers/StoreProvider';
 
