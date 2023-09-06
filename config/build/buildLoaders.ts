e30148f7b00b044587ed357091dfca25f166e95d
tusvel <tuswells@gmail.com>
2023-09-06 16:55:34 +0300

    6.46 React refresh plugin. build babel loader.

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,27 +2,12 @@ import type webpack from 'webpack';
 import {type BuildOptions} from './types/config';
 import {buildCssLoader} from './loaders/buildCssLoader';
 import {buildSvgLoader} from './loaders/buildSvgLoader';
+import {buildBabelLoader} from './loaders/buildBabelLoader';
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
-  const babel = {
-    test: /\.(js|jsx|ts|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
-  };
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const {isDev} = options;
+  
+  const babel = buildBabelLoader(options);
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
