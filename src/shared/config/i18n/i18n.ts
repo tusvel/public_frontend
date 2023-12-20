421828fc5681d3371ac2720479b36ecb91cef0aa
Alex Lev <tuswells@gmail.com>
2023-12-20 17:43:21 +0300

    9.62

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -4,20 +4,22 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
+void i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ru',
-    debug: __IS_DEV__,
+    fallbackLng: 'en',
+    // debug: __IS_DEV__,
+    debug: false,
+
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react as it escapes by default
     },
-  })
-  .then(
-    () => undefined,
-    () => undefined,
-  );
+
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  });
 
 export default i18n;
