6c37213ebac42c10ec5dd6a9c1e79f83c363d2ea
Alex Lev <tuswells@gmail.com>
2024-04-28 22:31:33 +0300

    14.8 add cypress test for routes

---
+++ cypress/e2e/common/routing.cy.ts
@@ -0,0 +1,23 @@
+import { selectByTestId } from '../../helpers/selectByTestId';
+
+describe('Роутинг', () => {
+  describe('Пользователь не авторизован', () => {
+    it('Переход на главную страницу', () => {
+      cy.visit('/');
+      cy.get(selectByTestId('MainPage')).should('exist');
+    });
+    it('Переход на страницу профиля', () => {
+      cy.visit('/profile/1');
+      cy.get(selectByTestId('MainPage')).should('exist');
+    });
+    it('Переход на несуществующий маршрут', () => {
+      cy.visit('/bdfsabfdsa');
+      cy.get(selectByTestId('NotFoundPage')).should('exist');
+    });
+  });
+  describe('Пользователь авторизован', () => {});
+  it('Переход на главную страницу', () => {
+    cy.visit('/');
+    cy.get(selectByTestId('MainPage')).should('exist');
+  });
+});
