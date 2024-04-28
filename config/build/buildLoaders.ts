45eb30c9941dcbfc079a4ce8f51ffdf4c2ccfa5f
Alex Lev <tuswells@gmail.com>
2024-04-28 16:30:46 +0300

    14.4. add ttf loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,17 +1,17 @@
 import type webpack from 'webpack';
-import {type BuildOptions} from './types/config';
-import {buildCssLoader} from './loaders/buildCssLoader';
-import {buildSvgLoader} from './loaders/buildSvgLoader';
-import {buildBabelLoader} from './loaders/buildBabelLoader';
+import { type BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildSvgLoader } from './loaders/buildSvgLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-  const {isDev} = options;
-  
-  const codeBabelLoader = buildBabelLoader({...options, isTsx: false});
-  const tsxCodeBabelLoader = buildBabelLoader({...options, isTsx: true});
+  const { isDev } = options;
+
+  const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
+  const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
 
   const fileLoader = {
-    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    test: /\.(png|jpe?g|gif|woff2|woff|ttf)$/i,
     use: [
       {
         loader: 'file-loader',
